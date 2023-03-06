8d8cb588ace74eb4552e6a7bd76c26d2948085a6
David Vílchez <dvilchez@xuaps.com>
2023-03-06 21:50:07 +0100

    Update config

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,1 +1,14 @@
-export default {};
+import "./style.css";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
